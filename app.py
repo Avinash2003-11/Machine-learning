e61156bf13ac7b5a39c28f042d4f6f7bb3f680ab
Avinash2003-11 <avinashmani2003@gmail.com>
2025-02-22 18:51:58 +0530

    Logistic Regression

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-st.title('Login page')
-st.write('Username')
-username = st.text_input('Enter your Username', key = "user_input")
-st.write('password')
-password = st.text_input('Enter your Password', type = 'password', key= 'pass_input')
-col1, col2 = st.columns([1,1])
-col1.button('login')
-col2.button("cancel")
